694cc50b7df448b9b7ecc63908ec4209e02f0272
quant-violinista <quant.violinista@gmail.com>
2020-09-30 14:39:04 -0500

    Implement pegasos

--- sentiment_analysis/project1.py
+++ sentiment_analysis/project1.py
@@ -235,16 +235,18 @@ def pegasos(feature_matrix, labels, T, L):
     number with the value of the theta_0, the offset classification
     parameter, found after T iterations through the feature matrix.
     """
-    # Your code here
-    raise NotImplementedError
+    theta = np.zeros((feature_matrix.shape[1],))
+    theta_0 = 0
+    updates = 0
+    for t in range(T):
+        for i in get_order(feature_matrix.shape[0]):
+            updates += 1
+            eta = 1/np.sqrt(updates)
+            theta, theta_0 = pegasos_single_step_update(feature_matrix[i], labels[i], L, eta, theta, theta_0)
+
+    return theta, theta_0
 
 
-# pragma: coderesponse end
-
-# Part II
-
-
-# pragma: coderesponse template
 def classify(feature_matrix, theta, theta_0):
     """
     A classification function that uses theta and theta_0 to classify a set of
