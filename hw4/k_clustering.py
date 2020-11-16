e8e4c30b5c3f6fd6866cc9c2e5cbb8cea7e4f63d
quant-violinista <quant.violinista@gmail.com>
2020-11-16 17:28:07 -0600

    HW 4 - Implement K Mediods clustering - Add unit test

--- hw4/k_clustering.py
+++ hw4/k_clustering.py
@@ -1,13 +1,15 @@
 import numpy as np
 import pandas as pd
+import random
 
 
 class K:
-    def __init__(self, x, z, norm):
+    def __init__(self, x, z, k, norm=2):
+        assert (z is not None) | (k is not None), 'Specify either k or initial representatives explicitly'
         self.x = x
-        self.z = z
+        self.z = z if z is not None else random.choices(x, k=k)
         self.norm = norm
-        self.k = len(z)
+        self.k = len(self.z)
         self.cost = None
         self.clusters = None
 
@@ -22,8 +24,8 @@ class K:
 
 
 class KMeans(K):
-    def __init__(self, x, z, norm=2):
-        super().__init__(x, z, norm)
+    def __init__(self, x, z=None, k=None, norm=2):
+        super().__init__(x, z, k, norm)
 
     def update_representatives(self):
         clusters = self.clusters.groupby(axis=0, level=0)
@@ -34,6 +36,33 @@ class KMeans(K):
 
     def cluster_data(self, iterations=1000, debug=True):
         assert self.norm in [1, 2], 'Only L1 and L2 norms allowed.'
+        for i in range(iterations):
+            self.assign_cluster()
+            self.update_representatives()
+        if debug:
+            print(f'The cost : {self.cost}')
+            print(f'The representatives are : {self.z}')
+            print(self.clusters)
+
+
+class KMediods(K):
+    def __init__(self, x, z=None, k=None, norm=2):
+        super().__init__(x, z, k, norm)
+
+    def update_representatives(self):
+        clusters = self.clusters.groupby(axis=0, level=0)
+        for i, group in clusters:
+            representative = self.z[i]
+            cost = 10000000.
+            for _, row in group.iterrows():
+                cost_row = np.sum(np.abs(row - group) ** self.norm, axis=1)
+                cost_row = np.sum(cost_row, axis=0)
+                if cost_row < cost:
+                    cost = cost_row
+                    representative = row
+            self.z[i] = representative
+
+    def cluster_data(self, iterations=1000, debug=True):
         for i in range(iterations):
             self.assign_cluster()
             self.update_representatives()
--- hw4/k_clustering_test.py
+++ hw4/k_clustering_test.py
@@ -1,13 +1,33 @@
 import unittest
 import numpy as np
-from k_clustering import KMeans
+from k_clustering import KMeans, KMediods
 
 
-class MyTestCase(unittest.TestCase):
-    def test_k_means(self):
+class ClusteringTest(unittest.TestCase):
+    def test_k_means_explicit(self):
         x = np.array([[0, -6], [4, 4], [0, 0], [-5, 2]], dtype=float)
         z = np.array([(-5, 2), (0, -6)], dtype=float)
-        k_object = KMeans(x, z, 1)
+        k_object = KMeans(x, z=z, norm=1)
+        k_object.cluster_data()
+        self.assertEqual(True, True)
+
+    def test_k_means_random(self):
+        x = np.array([[0, -6], [4, 4], [0, 0], [-5, 2]], dtype=float)
+        k_object = KMeans(x, k=2, norm=1)
+        k_object.cluster_data()
+        self.assertEqual(True, True)
+
+    def test_k_mediods_explicit(self):
+        x = np.array([[0, -6], [4, 4], [0, 0], [-5, 2]], dtype=float)
+        z = np.array([(-5, 2), (0, -6)], dtype=float)
+        k_object = KMediods(x, z=z, norm=1)
+        k_object.cluster_data()
+        self.assertEqual(True, True)
+
+    def test_k_mediods_explicit_euclidian(self):
+        x = np.array([[0, -6], [4, 4], [0, 0], [-5, 2]], dtype=float)
+        z = np.array([(-5, 2), (0, -6)], dtype=float)
+        k_object = KMediods(x, z=z, norm=2)
         k_object.cluster_data()
         self.assertEqual(True, True)
 
